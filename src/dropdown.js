6b72e969018e82136dd657947824830f6209e03c
momo934 <ask4r.safiullin@yandex.ru>
2023-03-29 18:02:51 +0300

    footer and dropdown #12

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,7 +1,7 @@
 const register_dropdowns = () => {
   const dropdowns = {};
   document.querySelectorAll(".dropdown").forEach((x) => {
-    dropdowns[x.dataset.name] = { element: x };
+    dropdowns[x.dataset.name] = { element: x, onMouse: false };
   });
   return dropdowns;
 };
@@ -15,58 +15,88 @@ const initDropdowns = () => {
     dropdowns: register_dropdowns(),
     initiators: register_dropdown_initiators(),
     cascade: [],
+    begin_pos: null,
     init() {
       this.initiators.forEach((initiator) => {
         initiator.addEventListener("mouseenter", (e) => {
+          if (this.cascade.length !== 0) {
+            this.cascade = [];
+            this.handleClose();
+          }
           const to = initiator.dataset.to;
           this.cascade.push(to);
-          this.handleOpen(initiator);
+          this.begin_pos = initiator.getBoundingClientRect().left;
+          this.handleOpen();
+          initiator.classList.add("dropdown-active");
         });
+
         initiator.addEventListener("mouseleave", () => {
-          this.cascade.pop()
-          this.handleClose()
-        })
+          initiator.classList.remove("dropdown-active");
+        });
       });
+
+      document
+        .querySelectorAll(".dropdown-initiator-cascade")
+        .forEach((initiator) => {
+          initiator.addEventListener("mouseenter", (e) => {
+            const to = e.target.dataset.to;
+            this.cascade.push(to);
+            this.handleOpen();
+            
+          });
+          initiator.addEventListener("mouseleave", (e) => {
+            const to = e.target.dataset.to;
+            // console.log(this.onDropdown(to));
+            // if(!this.onDropdown(to)){
+            //   this.handleOpen();
+            //   this.cascade.pop()
+            // }
+          });
+        });
 
       Object.keys(this.dropdowns).forEach((x) => {
         const dropdown = this.dropdowns[x];
+
         dropdown.element.addEventListener("mouseenter", () => {
           this.dropdowns[x].onMouse = true;
         });
-      });
 
-      Object.keys(this.dropdowns).forEach((x) => {
-        const dropdown = this.dropdowns[x];
         dropdown.element.addEventListener("mouseleave", () => {
           this.dropdowns[x].onMouse = false;
-          const next = this.cascade.pop();
-          if (this.dropdowns[this.cascade.at(-1)]?.onMouse) {
-            next.style.display = "none";
-          }else{
-            this.cascade = []
-            this.handleClose()
-          }
         });
+      });
+      document.addEventListener("click", () => {
+        if (this.onAnyDropdown()) return;
+        this.handleClose();
+        this.cascade = [];
       });
     },
     handleOpen() {
       const header_rect = document
         .querySelector(".header-nav")
         .getBoundingClientRect();
-      console.log(this.cascade);
-      const dropdown = this.dropdowns[this.cascade.at(-1)];
 
-      dropdown.element.style.cssText = `
-        left: ${header_rect.left + (390 * (this.cascade.length - 1))}px;
-        display: block; 
-        top: ${header_rect.top + header_rect.height}px
-        `;
-    },
-    handleClose(){
-      Object.values(this.dropdowns).forEach((x) => {
-        console.log(x);
-       x.element.style = ''
+      this.cascade.forEach((x, index) => {
+        const dropdown = this.dropdowns[x];
+
+        dropdown.element.style.cssText = `
+            left: ${this.begin_pos + 390 * index}px;
+            display: block; 
+            top: ${header_rect.top + header_rect.height}px
+            `;
       });
-    }
+    },
+    handleClose() {
+      Object.values(this.dropdowns).forEach((x) => {
+        x.element.style = "";
+      });
+      this.beginPos = null;
+    },
+    onAnyDropdown() {
+      return false;
+    },
+    onDropdown(dropdown_name) {
+      return this.dropdowns[dropdown_name].onMouse;
+    },
   };
 };
