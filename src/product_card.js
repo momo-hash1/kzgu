5f122f4c2e1c20169114df0b262db24e7c4ef289
momo934 <ask4r.safiullin@yandex.ru>
2023-04-01 14:35:56 +0300

    fix product card description

--- src/product_card.js
+++ src/product_card.js
@@ -28,6 +28,16 @@ const header_items = document.querySelectorAll(
   ".product-description-header .item"
 );
 
+product_description.on("slideChange", (e) => {
+  header_items.forEach((x, index_x) => {
+    if (e.activeIndex == index_x) {
+      x.classList.add("item-active");
+    } else {
+      x.classList.remove("item-active");
+    }
+  });
+})
+
 header_items.forEach((product_item, index) => {
   product_item.addEventListener("click", () => {
     header_items.forEach((x, index_x) => {
