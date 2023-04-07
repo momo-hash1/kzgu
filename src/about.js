c5998089e0ceef88fe0293d7bd92246ff9624b31
momo934 <ask4r.safiullin@yandex.ru>
2023-04-07 08:08:54 +0300

    about page fix #24 #23 #22

--- src/about.js
+++ src/about.js
@@ -1,12 +1,28 @@
 import Swiper from "swiper";
 import initGallery from "./ui/gallery";
-import './style/pages/about.scss'
+import "./style/pages/about.scss";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
 
 const reviewSwiper = new Swiper(".partner-review-swiper", {
   direction: "horizontal",
   slidesPerView: 1,
   simulateTouch: false,
 });
+
+const partnerSwiper = new Swiper(".partner-info-swiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  allowTouchMove: false
+});
+
+reviewSwiper.on("slideChange", (e) => {
+  partnerSwiper.slideTo(e.activeIndex)
+})
 
 document
   .querySelector(".swiper-header-review-prev")
@@ -15,4 +31,25 @@ document
   .querySelector(".swiper-header-review-next")
   .addEventListener("click", () => reviewSwiper.slideNext());
 
-initGallery("swiper-gallery")
+initGallery("swiper-gallery");
+
+const video_window = document.querySelector(".video video");
+const video_parent = document.querySelector(".watch-video");
+
+document.querySelector(".play-button").addEventListener("click", () => {
+  video_parent.classList.add("show-video");
+  video_window.play();
+});
+
+let its_seeked = false;
+
+video_window.addEventListener("pause", () => {
+  if (video_window.seeking) return;
+  video_parent.classList.remove("show-video");
+});
+video_window.addEventListener("seeking", () => {
+  its_seeked = true;
+  console.log(video_window.paused);
+});
+
+video_window.addEventListener("seeked", () => {});
