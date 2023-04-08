b0f5ffbb383c786a5fa70d778c98097b54983342
momo934 <ask4r.safiullin@yandex.ru>
2023-04-08 18:32:14 +0300

    init all sliders

--- src/catalog.js
+++ src/catalog.js
@@ -2,21 +2,18 @@ import noUiSlider from "nouislider";
 import initAccordion from "./ui/accordion";
 import menu_init from "./ui/modal";
 import initModalWindow from "./ui/modal_window";
-
-var slider = document.getElementById("slider");
 import "./style/pages/catalog.scss";
 
-document.addEventListener("DOMContentLoaded", () => {
-  let maximum_price = parseInt(slider.dataset.maximum_price);
-  let minimum_price = parseInt(slider.dataset.minimum_price);
-  if (isNaN(maximum_price) ) {
-    maximum_price = 100
-  };
-  if(isNaN(minimum_price)){
-    minimum_price = 0
+const register_slider = (slider_element) => {
+  let maximum_price = parseInt(slider_element.dataset.maximum_price);
+  let minimum_price = parseInt(slider_element.dataset.minimum_price);
+  if (isNaN(maximum_price)) {
+    maximum_price = 100;
   }
-  console.log(minimum_price);
-  noUiSlider.create(slider, {
+  if (isNaN(minimum_price)) {
+    minimum_price = 0;
+  }
+  const slider_main = noUiSlider.create(slider_element, {
     start: [minimum_price, maximum_price],
     connect: true,
     range: {
@@ -24,23 +21,27 @@ document.addEventListener("DOMContentLoaded", () => {
       max: [maximum_price],
     },
   });
-  const slider_bottom = document.querySelector("#slider-bottom");
+  const slider_bottom = document.querySelector(
+    `[data-for="${slider_element.dataset.sliderName}"]`
+  );
+
   const start_input = slider_bottom.querySelector(".start");
+
   const end_input = slider_bottom.querySelector(".end");
 
-  slider.noUiSlider.on("update", () => {
-    const range = slider.noUiSlider.get();
+  slider_main.on("update", () => {
+    const range = slider_main.get();
     start_input.value = `От ${Math.trunc(range[0])}`;
     end_input.value = `До ${Math.trunc(range[1])}`;
   });
 
   start_input.addEventListener("click", () => {
-    start_input.value = Math.trunc(slider.noUiSlider.get()[0]);
+    start_input.value = Math.trunc(slider_main.get()[0]);
     start_input.select();
   });
 
   end_input.addEventListener("click", () => {
-    end_input.value = Math.trunc(slider.noUiSlider.get()[1]);
+    end_input.value = Math.trunc(slider_main.get()[1]);
     end_input.select();
   });
 
@@ -48,20 +49,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const target_class = e.target.classList;
     const contains_essential_classes =
       target_class.contains("end") || target_class.contains("start");
+
     const isInput = e.target.tagName.toLowerCase() === "input";
+
     if (contains_essential_classes && isInput) return;
 
-    slider.noUiSlider.set([start_input.value, end_input.value]);
-    start_input.value = `От ${Math.trunc(slider.noUiSlider.get()[0])}`;
-    end_input.value = `До ${Math.trunc(slider.noUiSlider.get()[1])}`;
+    slider_main.set([start_input.value, end_input.value]);
+    start_input.value = `От ${Math.trunc(slider_main.get()[0])}`;
+    end_input.value = `До ${Math.trunc(slider_main.get()[1])}`;
   });
 
   end_input.addEventListener("keydown", (e) => {
     if (e.key !== "Enter") return;
 
     slider.noUiSlider.set([start_input.value, end_input.value]);
-    start_input.value = `От ${Math.trunc(slider.noUiSlider.get()[0])}`;
-    end_input.value = `До ${Math.trunc(slider.noUiSlider.get()[1])}`;
+    start_input.value = `От ${Math.trunc(slider_main.get()[0])}`;
+    end_input.value = `До ${Math.trunc(slider_main.get()[1])}`;
 
     end_input.blur();
   });
@@ -70,10 +73,20 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.key !== "Enter") return;
 
     slider.noUiSlider.set([start_input.value, end_input.value]);
-    start_input.value = `От ${Math.trunc(slider.noUiSlider.get()[0])}`;
-    end_input.value = `До ${Math.trunc(slider.noUiSlider.get()[1])}`;
+    start_input.value = `От ${Math.trunc(slider_main.get()[0])}`;
+    end_input.value = `До ${Math.trunc(slider_main.get()[1])}`;
 
     start_input.blur();
+  });
+  return slider;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sliders = [...document.querySelectorAll(".slider")];
+  sliders.map((slider) => {
+    if (slider.dataset.sliderName) {
+      return register_slider(slider);
+    }
   });
 });
 
